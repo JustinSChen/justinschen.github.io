a56845d9770cf28b7cc22825767c723012340808
Justin Chen <justinsengchen@gmail.com>
2020-03-11 00:33:41 -0700

    Initial Master

---
+++ precache-manifest.992f3f79f57d9d41ab1b30fbb3f95fd1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94b374e6b0fb2b3831db58c37bd78862",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a46118fc305de478f39",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bcea32b16f4080957179",
+    "url": "/static/js/2.c16e3ea4.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.c16e3ea4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a46118fc305de478f39",
+    "url": "/static/js/main.028a44f3.chunk.js"
+  },
+  {
+    "revision": "de730f1838e6e654efd1",
+    "url": "/static/js/runtime-main.5c1e1577.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
